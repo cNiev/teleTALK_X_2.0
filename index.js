c5f0fcbbec10fdf71d38f72f77e842ad5a508ca4
Carlos Nieves <82455016+cNiev@users.noreply.github.com>
2025-01-13 12:16:27 -0600

    Update index.js

--- index.js
+++ index.js
@@ -10,22 +10,23 @@ if (!process.env.TWITTER_ACCESS_TOKEN_SECRET) throw new Error('Falta TWITTER_ACC
 const twitterClient = new TwitterApi({
   appKey: process.env.TWITTER_API_KEY,
   appSecret: process.env.TWITTER_API_SECRET,
-  accessToken: process.env.TWITTER_ACCESS_TOKEN,
-  accessSecret: process.env.TWITTER_ACCESS_TOKEN_SECRET,
 });
+
+// Autenticación Application-Only
+const appOnlyClient = await twitterClient.appLogin();
 
 (async () => {
   try {
     // Lógica principal: Obtener conteo de tweets con el hashtag #arte
     const hashtag = '#arte';
-    const counts = await twitterClient.v2.tweetCountRecent({ query: hashtag });
+    const counts = await appOnlyClient.v2.tweetCountRecent({ query: hashtag });
     const totalTweets = counts.meta.total_tweet_count;
 
     // Construir el texto del tweet
     const textoTweet = `En las últimas 24 horas, el hashtag ${hashtag} fue usado ${totalTweets} veces. 🌟 ¡El arte sigue inspirando al mundo!`;
 
     // Publicar el tweet
-    const tweet = await twitterClient.v2.tweet(textoTweet);
+    const tweet = await appOnlyClient.v2.tweet(textoTweet);
     console.log('Tweet publicado con éxito:', tweet);
   } catch (error) {
     console.error('Error al procesar el conteo o publicar el tweet:', error);
